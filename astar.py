7909b2cbdaef8e3f8239afec2f4a0c62baa1a2ee
Lope Emano <lopegwapo@gmail.com>
2013-12-12 21:34:51 +0800

    added input.txt and integrated with astar search

--- astar.py
+++ astar.py
@@ -4,9 +4,10 @@ import math
 
 class Node:
     distance = None
-    def __init__(self, x, y, board):
+    def __init__(self, x, y, value):
         self.x = x
         self.y = y
+        self.value = value
 
     def get_neighbors(self):
         neighbors = []
@@ -24,8 +25,17 @@ class Board:
 
     def __init__(self, rows, columns):
         self.matrix = [[None]*rows for i in range(columns)]
+
+    def fill(self, rows):
+        for (i, row) in enumerate(rows):
+            for (j, item) in enumerate(row):
+                node = Node(x=i, y=j, value=int(item))
+                self.matrix[i][j] = node
+        return self.matrix 
+
     def set_node(self, x, y, node):
         self.matrix[x][y] = node 
+
     def get_node(self, x, y):
         return self.matrix[x][y] 
 
@@ -37,10 +47,11 @@ class AStarSearch:
         self.end_node = end_node
         self.current_node = start_node
 
-    def start(self):
+    def search(self):
         path = []
         while (self._compute_distance(self.current_node, self.end_node) != 0):
             path.append(self._get_nearest_neighbor(self.current_node))
+        return path
 
     def _get_nearest_neighbor(self, node):
         neighbors = self.node.get_neighbors()
@@ -87,6 +98,31 @@ class AStarSearch:
                 return cost_lower_right
         raise Exception('cost not found exception')
 
+class Runner():
+    def __init__(self):
+        self.board = None
+        self.search = None
+
+    def read_file(self):
+        import pdb
+        with open('input.txt','r') as file:
+            rows = []
+            x, y = 0, 0
+            for (i, line) in enumerate(file):
+                if i == 0:
+                    x, y = line.split()
+                    x, y = int(x), int(y)
+                    self.board = Board(x, y)
+                elif i <= y:
+                    rows.append(line.split())
+                else:
+                    print line
+            self.board.fill(rows)
+        pdb.set_trace()
+import pdb
+runner = Runner()
+runner.read_file()
+pdb.set_trace()   
 board = Board(5,5)
 board.get_node(2,2)
 pdb.set_trace()
