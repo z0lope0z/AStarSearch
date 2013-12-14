d1e17093673edec04522c0ae27010990e6b3aab0
Lope Emano <lopegwapo@gmail.com>
2013-12-14 13:04:16 +0800

    fixed bugs, did first traverse

--- astar.py
+++ astar.py
@@ -1,25 +1,46 @@
 import pdb
 import math
+import time
 
 
 class Node:
-    distance = None
-    def __init__(self, x, y, value):
+    current_cost = None
+
+    def __init__(self, x, y, value, board):
         self.x = x
         self.y = y
+        self.current_cost = 0
         self.value = value
+        self.board = board
 
     def get_neighbors(self):
+        neighbors = self._get_neighbors_unclean(self)
+        return_neighbors = []
+        for neighbor in neighbors:
+            try:
+                node = self.board.get_node(neighbor.x, neighbor.y)
+                if node.value != 1 and neighbor != None:
+                    return_neighbors.append(node)
+            except AttributeError:
+                pass
+            except IndexError:
+                pass
+        return return_neighbors
+
+    def _get_neighbors_unclean(self, neighbors):
         neighbors = []
-        neighbors.append(board.get_node(x-1, y-1))
-        neighbors.append(board.get_node(x-1, y))
-        neighbors.append(board.get_node(x-1, y+1))
-        neighbors.append(board.get_node(x, y+1))
-        neighbors.append(board.get_node(x+1, y+1))
-        neighbors.append(board.get_node(x+1, y))
-        neighbors.append(board.get_node(x+1, y-1))
-        neighbors.append(board.get_node(x, y-1))
+        neighbors.append(self.board.get_node(self.x-1, self.y+1))
+        neighbors.append(self.board.get_node(self.x-1, self.y))
+        neighbors.append(self.board.get_node(self.x-1, self.y-1))
+        neighbors.append(self.board.get_node(self.x, self.y+1))
+        neighbors.append(self.board.get_node(self.x+1, self.y+1))
+        neighbors.append(self.board.get_node(self.x+1, self.y))
+        neighbors.append(self.board.get_node(self.x+1, self.y-1))
+        neighbors.append(self.board.get_node(self.x, self.y-1))                                          
         return neighbors
+
+    def __str__(self):
+        return "%d,%d" % (self.x, self.y)
 
 class Board:
 
@@ -29,7 +50,7 @@ class Board:
     def fill(self, rows):
         for (i, row) in enumerate(rows):
             for (j, item) in enumerate(row):
-                node = Node(x=i, y=j, value=int(item))
+                node = Node(x=j, y=i, value=int(item), board=self)
                 self.matrix[i][j] = node
         return self.matrix 
 
@@ -37,7 +58,11 @@ class Board:
         self.matrix[x][y] = node 
 
     def get_node(self, x, y):
-        return self.matrix[x][y] 
+        try:
+            return self.matrix[x][y]
+        except:
+            return None
+    
 
 class AStarSearch:
     cost_upper_left, cost_left, cost_lower_left, cost_bottom, cost_lower_right, cost_right, cost_upper_right, cost_top = 1, 1, 1, 1, 1, 1, 1, 1
@@ -46,68 +71,103 @@ class AStarSearch:
         self.start_node = start_node
         self.end_node = end_node
         self.current_node = start_node
+        self.open_nodes = []
 
     def search(self):
         path = []
         while (self._compute_distance(self.current_node, self.end_node) != 0):
-            path.append(self._get_nearest_neighbor(self.current_node))
+            path.append(self.current_node)
+            self.current_node, is_shift = self._check_open_nodes()
+            nearest_node = self._get_nearest_neighbor(self.current_node)
+            self._move(nearest_node)
+            print "moved to %d,%d" % (nearest_node.x, nearest_node.y)
+            time.sleep(1)
         return path
+    
+    def _move(self, node):
+        movement_cost = self._movement_cost(self.current_node, node) 
+        self.current_node = node
+        self.current_node.current_cost = movement_cost  
+
+    def _check_open_nodes(self):
+        cheapest_node = self.current_node
+        is_shift = False
+        for node in self.open_nodes:
+            if self.current_node.current_cost < node.current_cost:
+                cheapest_node = node
+                is_shift = True
+        return cheapest_node, is_shift 
 
     def _get_nearest_neighbor(self, node):
-        neighbors = self.node.get_neighbors()
-        if neighbors:
-            nearest_neighbor = neighbors[0]
-            for neighbor in neighbors:
-                distance = self._compute_distance(node, neighbor)
-                if distance < nearest_neighbor.distance:
-                    nearest_neighbor = neighbor
+        neighbors = node.get_neighbors()
+        print "node: %s,%s" % (node.x, node.y)
+        try:
+            if neighbors:
+                nearest_neighbor = neighbors[0]
+                for neighbor in neighbors:
+                    print "%s,%s cost: %f value: %d" % (neighbor.x, neighbor.y, self._compute_distance(node, neighbor), neighbor.value)
+                    distance = self._compute_distance(node, neighbor)
+                    if distance < self._compute_distance(node, nearest_neighbor):
+                        nearest_neighbor = neighbor
+        except Exception, e:
+            pdb.set_trace()
         return nearest_neighbor
 
     def _compute_distance(self, node, neighbor):
         movement_cost = self._movement_cost(node, neighbor)
         if movement_cost == 0:
             return 0
-        return movement_cost + self._heuristic(neighbor) + self.current_node.current_cost
+        comp = movement_cost + self._heuristic(neighbor) + self.current_node.current_cost
+        return comp
 
     def _heuristic(self, node):
         x_diff = abs(node.x-self.end_node.x)
         y_diff = abs(node.y-self.end_node.y)
-        return math.sqrt(math.pow(x_diff, 2), math.pow(y_diff, 2))
+        return math.sqrt(math.pow(x_diff, 2) + math.pow(y_diff, 2))
 
     def _movement_cost(self, node, neighbor):
         if node.x > neighbor.x:
             if neighbor.y > node.y:
-                return cost_upper_left
+                return self.cost_upper_left
             elif neighbor.y == node.y:
-                return cost_left
+                return self.cost_left
             elif neighbor.y < node.y:
-                return cost_right
+                return self.cost_right
         elif node.x == neighbor.x:
             if neighbor.y > node.y:
-                return cost_top
+                return self.cost_top
             elif neighbor.y == node.y:
                 return 0
             elif neighbor.y < node.y:
-                return cost_bottom 
-        elif node.x < neighbor.y:
+                return self.cost_bottom 
+        elif node.x < neighbor.x:
             if neighbor.y > node.y:
-                return cost_upper_right
+                return self.cost_upper_right
             elif neighbor.y == node.y:
-                return cost_right
+                return self.cost_right
             elif neighbor.y < node.y:
-                return cost_lower_right
+                return self.cost_lower_right
+        pdb.set_trace()
         raise Exception('cost not found exception')
+
+    def set_movement_cost(self, left, down, right, up, diagonal):
+        self.cost_upper_left, self.cost_upper_right, self.cost_lower_left, self.cost_lower_right = diagonal, diagonal, diagonal, diagonal
+        self.cost_left, self.cost_down, self.cost_right, self.cost_up = left, down, right, up 
 
 class Runner():
     def __init__(self):
         self.board = None
-        self.search = None
+        self.a_star_search = None
+        self.greedy_search = None
 
     def read_file(self):
         import pdb
         with open('input.txt','r') as file:
             rows = []
             x, y = 0, 0
+            start_node = None
+            end_node = None
+            cost_up, cost_down, cost_left, cost_right, cost_diagonal = None, None, None, None, None
             for (i, line) in enumerate(file):
                 if i == 0:
                     x, y = line.split()
@@ -117,9 +177,33 @@ class Runner():
                     rows.append(line.split())
                 else:
                     print line
-            self.board.fill(rows)
+                    self.board.fill(rows)
+                    if line.startswith('Source'):
+                        x, y = self._convert_coordinates(int(line.split()[1]), int(line.split()[2]))
+                        start_node = self.board.get_node(x, y)
+                    elif line.startswith('Destination'):
+                        x, y = self._convert_coordinates(int(line.split()[1]), int(line.split()[2]))
+                        end_node = self.board.get_node(x, y)
+                    elif line.startswith('Up'):
+                        cost_up = int(line.split()[1])
+                    elif line.startswith('Down'):
+                        cost_down = int(line.split()[1])
+                    elif line.startswith('Left'):
+                        cost_left = int(line.split()[1])
+                    elif line.startswith('Right'):
+                        cost_right = int(line.split()[1])
+                    elif line.startswith('Diagonal'):
+                        cost_diagonal = int(line.split()[1])
+        self.a_star_search = AStarSearch(start_node, end_node)
+        self.a_star_search.set_movement_cost(cost_up, cost_down, cost_left, cost_right, cost_diagonal)
+        path = self.a_star_search.search()
+        for node in path:
+            print('%d,%d' % (node.x, node.y)),
         pdb.set_trace()
-import pdb
+    
+    def _convert_coordinates(self, x, y):
+        return y, x
+
 runner = Runner()
 runner.read_file()
 pdb.set_trace()   
